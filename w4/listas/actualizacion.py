f11dd70f5738e7f6c0d13500885c0a744b9c75a8
Jose Enrique Avila <senquevila@gmail.com>
2024-10-29 08:47:35 -0600

    uso de json para serializar la lista en el fichero

--- w4/listas/actualizacion.py
+++ w4/listas/actualizacion.py
@@ -1,23 +1,28 @@
+import json
+
+
 def cargar_lista() -> list:
     with open("datos.txt") as f:
-        s = f.read()
-        return s[1 : len(s) - 1].split(", ")
+        data = json.loads(f.read())
+        return data
 
 
+def guardar_lista(lista: list):
+    with open("datos.txt", "w") as f:
+        f.write(json.dumps(lista))
+
+
+# Ejecucion principal
 a = cargar_lista()
 try:
     posicion = int(input("Escriba la posicion: "))
     valor = input("Coloque el nuevo valor: ")
     a[posicion] = valor
-    print(a)
 
-    with open("datos.txt", "w") as archivo:
-        archivo.write(str(a))
+    print(a)
+    guardar_lista(a)
 
 except IndexError:
     print(f"La posicion debe estar entre 0 y {len(a)}")
 except ValueError:
     print(f"La posicion debe ser un numero entero 0 y {len(a)}")
-    
-
-
